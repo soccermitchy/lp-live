28139d3cfd9020edc4ee652ff028cd27898ccc2b
Mitchell Monahan <soccermitchy@gmail.com>
2013-09-04 21:42:22 -0400

    Script to create directories needed for lb build.

---
+++ mkdirs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir config/apt
+mkdir config/hooks
+mkdir config/includes.binary
+mkdir config/includes.debian-installer
+mkdir packages
+mkdir packages.chroot
+mkdir rootfs
+mkdir debian-installer
+mkdir includes
+mkdir includes.chroot
+mkdir packages.binary
+mkdir preseed
